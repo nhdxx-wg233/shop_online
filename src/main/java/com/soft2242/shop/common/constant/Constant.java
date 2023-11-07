f896343646e8a57a978d6958c1d2a4ca31bd657e
wg233 <18851716394@qq.com>
2023-11-07 12:57:09 +0800

    feat:搭建基础框架

---
+++ src/main/java/com/soft2242/shop/common/constant/Constant.java
@@ -0,0 +1,10 @@
+package com.soft2242.shop.common.constant;
+
+public class Constant {
+    //    创建时间
+    String CREATE_TIME = "createTime";
+    //    更新时间
+    String UPDATE_TIME = "updateTime";
+    //    逻辑删除
+    String DELETE_FLAG = "deleteFlag";
+}
