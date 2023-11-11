eb5d4ee0740530690d43c145630068ea7816d91c
wg-233 <18851716394@qq.com>
2023-11-11 15:56:10 +0800

    feat: 商品分类列表

--- src/main/java/com/soft2242/shop/service/CategoryService.java
+++ src/main/java/com/soft2242/shop/service/CategoryService.java
@@ -2,6 +2,7 @@ package com.soft2242.shop.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.soft2242.shop.entity.Category;
+import com.soft2242.shop.vo.CategoryVO;
 
 import java.util.List;
 
@@ -15,4 +16,5 @@ import java.util.List;
  */
 public interface CategoryService extends IService<Category> {
     List<Category> getIndexCategoryList();
+    List<CategoryVO> getCategoryList();
 }
--- src/main/java/com/soft2242/shop/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/soft2242/shop/service/impl/CategoryServiceImpl.java
@@ -2,14 +2,20 @@ package com.soft2242.shop.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.soft2242.shop.convert.GoodsConvert;
 import com.soft2242.shop.entity.Category;
+import com.soft2242.shop.entity.Goods;
 import com.soft2242.shop.enums.CategoryRecommendEnum;
 import com.soft2242.shop.mapper.CategoryMapper;
 import com.soft2242.shop.mapper.GoodsMapper;
 import com.soft2242.shop.service.CategoryService;
+import com.soft2242.shop.vo.CategoryChildrenGoodsVO;
+import com.soft2242.shop.vo.CategoryVO;
+import com.soft2242.shop.vo.RecommendGoodsVO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,6 +37,41 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> i
         wrapper.eq(Category::getIsRecommend, CategoryRecommendEnum.ALL_RECOMMEND.getValue());
         wrapper.orderByDesc(Category::getCreateTime);
         List<Category> list = baseMapper.selectList(wrapper);
+        return list;
+    }
+
+    @Override
+    public List<CategoryVO> getCategoryList() {
+        ArrayList<CategoryVO> list = new ArrayList<>();
+        LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Category::getIsRecommend, CategoryRecommendEnum.ALL_RECOMMEND.getValue()).or();
+        List<Category> categories = baseMapper.selectList(wrapper);
+        LambdaQueryWrapper<Goods> queryWrapper = new LambdaQueryWrapper<>();
+        for (Category category : categories) {
+            CategoryVO categoryVO = new CategoryVO();
+            categoryVO.setId(category.getId());
+            categoryVO.setName(category.getName());
+            categoryVO.setIcon(category.getIcon());
+            wrapper.clear();
+            wrapper.eq(Category::getParentId, category.getId());
+            List<Category> childCategories = baseMapper.selectList(wrapper);
+            ArrayList<CategoryChildrenGoodsVO> categoryChildrenGoodsList = new ArrayList<>();
+            for (Category item : childCategories) {
+                CategoryChildrenGoodsVO childrenGoodsVO = new CategoryChildrenGoodsVO();
+                childrenGoodsVO.setId(item.getId());
+                childrenGoodsVO.setName(item.getName());
+                childrenGoodsVO.setIcon(item.getIcon());
+                childrenGoodsVO.setParentId(category.getId());
+                childrenGoodsVO.setParentName(category.getName());
+                queryWrapper.clear();
+                List<Goods> goodsList = goodsMapper.selectList(queryWrapper.eq(Goods::getCategoryId, item.getId()));
+                List<RecommendGoodsVO> goodsVOList = GoodsConvert.INSTANCE.convertToRecommendGoodsVOList(goodsList);
+                childrenGoodsVO.setGoods(goodsVOList);
+                categoryChildrenGoodsList.add(childrenGoodsVO);
+            }
+            categoryVO.setChildren(categoryChildrenGoodsList);
+            list.add(categoryVO);
+        }
         return list;
     }
 }
