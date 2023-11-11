3a75423e268df52ca02195118529cc712de4e191
wg-233 <18851716394@qq.com>
2023-11-11 22:39:02 +0800

    fix(地址管理模块): 修复添加重复地址的问题

--- src/main/java/com/soft2242/shop/service/impl/UserShoppingAddressServiceImpl.java
+++ src/main/java/com/soft2242/shop/service/impl/UserShoppingAddressServiceImpl.java
@@ -6,13 +6,13 @@ import com.soft2242.shop.common.exception.ServerException;
 import com.soft2242.shop.convert.AddressConvert;
 import com.soft2242.shop.entity.UserShoppingAddress;
 import com.soft2242.shop.enums.AddressDefaultEnum;
-import com.soft2242.shop.enums.AddressDeleteFlagEnum;
 import com.soft2242.shop.mapper.UserShoppingAddressMapper;
 import com.soft2242.shop.service.UserShoppingAddressService;
 import com.soft2242.shop.vo.AddressVO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * <p>
@@ -28,10 +28,36 @@ public class UserShoppingAddressServiceImpl extends ServiceImpl<UserShoppingAddr
     @Override
     public Integer saveShoppingAddress(AddressVO addressVO) {
         UserShoppingAddress convert = AddressConvert.INSTANCE.convert(addressVO);
-        if (convert.getIsDefault() == AddressDefaultEnum.DEFAULT_ADDRESS.getValue()){
-            List<UserShoppingAddress> list = baseMapper.selectList(new LambdaQueryWrapper<UserShoppingAddress>().eq(UserShoppingAddress::getIsDefault, AddressDefaultEnum.DEFAULT_ADDRESS.getValue()));
-            if (list.size() > 0){
-             throw new ServerException("已经存在默认地址,请勿重复操作");
+
+        UserShoppingAddress existingAddress = baseMapper.selectOne(new LambdaQueryWrapper<UserShoppingAddress>()
+                .eq(UserShoppingAddress::getUserId, addressVO.getUserId())
+                .eq(UserShoppingAddress::getReceiver, addressVO.getReceiver())
+                .eq(UserShoppingAddress::getContact, addressVO.getContact())
+                .eq(UserShoppingAddress::getCityCode, addressVO.getCityCode())
+                .eq(UserShoppingAddress::getCountyCode, addressVO.getCountyCode())
+                .eq(UserShoppingAddress::getProvinceCode, addressVO.getProvinceCode())
+                .eq(UserShoppingAddress::getAddress, addressVO.getAddress()));
+        if (existingAddress != null) {
+            throw new ServerException("该地址已存在，请勿重复添加");
+        }
+        // 先进行一次查询，获取当前的默认地址列表
+        List<UserShoppingAddress> defaultAddresses = baseMapper.selectList(new LambdaQueryWrapper<UserShoppingAddress>()
+                .eq(UserShoppingAddress::getIsDefault, AddressDefaultEnum.DEFAULT_ADDRESS.getValue())
+                .eq(UserShoppingAddress::getUserId, addressVO.getUserId()));
+
+        if (Objects.equals(convert.getIsDefault(), AddressDefaultEnum.DEFAULT_ADDRESS.getValue())) {
+            // 检查该地址是否已经在默认地址列表中
+            if (defaultAddresses.stream().anyMatch(address -> address.getId().equals(addressVO.getId()))) {
+                throw new ServerException("该地址已经是默认地址,请勿重复修改");
+            }
+            // 如果存在其他默认地址，则将 convert 设置为非默认地址并抛出异常
+            if (!defaultAddresses.isEmpty()) {
+                throw new ServerException("已存在默认地址,无法将该地址设置为默认地址");
+            }
+        } else {
+            // 如果不存在默认地址，则将 convert 设置为默认地址
+            if (defaultAddresses.isEmpty()) {
+                convert.setIsDefault(AddressDefaultEnum.DEFAULT_ADDRESS.getValue());
             }
         }
         save(convert);
@@ -44,7 +70,7 @@ public class UserShoppingAddressServiceImpl extends ServiceImpl<UserShoppingAddr
         if (userShoppingAddress == null){
             throw new ServerException("地址不存在");
         }
-        if (addressVO.getIsDefault() == AddressDefaultEnum.DEFAULT_ADDRESS.getValue()){
+        if (Objects.equals(addressVO.getIsDefault(), AddressDefaultEnum.DEFAULT_ADDRESS.getValue())) {
             UserShoppingAddress address = baseMapper.selectOne(new LambdaQueryWrapper<UserShoppingAddress>().eq(UserShoppingAddress::getUserId, addressVO.getUserId()).eq(UserShoppingAddress::getIsDefault, AddressDefaultEnum.DEFAULT_ADDRESS.getValue()));
             if (address != null){
                 address.setIsDefault(AddressDefaultEnum.NOT_DEFAULT_ADDRESS.getValue());
@@ -74,7 +100,7 @@ public class UserShoppingAddressServiceImpl extends ServiceImpl<UserShoppingAddr
         UserShoppingAddress address = baseMapper.selectById(id);
         if (address == null){
             throw new ServerException("地址不存在");
-        }else if (address.getIsDefault() == AddressDefaultEnum.DEFAULT_ADDRESS.getValue()){
+        } else if (Objects.equals(address.getIsDefault(), AddressDefaultEnum.DEFAULT_ADDRESS.getValue())) {
             throw new ServerException("默认地址不能删除");
         }else {
             baseMapper.deleteById(id);
