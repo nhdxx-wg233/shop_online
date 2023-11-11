7e3318bfac6a1bc2669ffaf56a8e2c43cd8f4de0
wg-233 <18851716394@qq.com>
2023-11-11 14:46:18 +0800

    feat: 首页轮播

--- src/main/java/com/soft2242/shop/service/IndexCarouselService.java
+++ src/main/java/com/soft2242/shop/service/IndexCarouselService.java
@@ -3,6 +3,8 @@ package com.soft2242.shop.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.soft2242.shop.entity.IndexCarousel;
 
+import java.util.List;
+
 /**
  * <p>
  * 服务类
@@ -12,5 +14,11 @@ import com.soft2242.shop.entity.IndexCarousel;
  * @since 2023-11-07
  */
 public interface IndexCarouselService extends IService<IndexCarousel> {
-
+    /**
+     * 获取指定分布点的轮播图列表
+     *
+     * @param distributionSite 分布点的标识
+     * @return 分布点对应的轮播图列表
+     */
+    List<IndexCarousel> getList(Integer distributionSite);
 }
--- src/main/java/com/soft2242/shop/service/impl/IndexCarouselServiceImpl.java
+++ src/main/java/com/soft2242/shop/service/impl/IndexCarouselServiceImpl.java
@@ -1,10 +1,13 @@
 package com.soft2242.shop.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.soft2242.shop.entity.IndexCarousel;
 import com.soft2242.shop.mapper.IndexCarouselMapper;
 import com.soft2242.shop.service.IndexCarouselService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +20,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class IndexCarouselServiceImpl extends ServiceImpl<IndexCarouselMapper, IndexCarousel> implements IndexCarouselService {
 
+    @Override
+    public List<IndexCarousel> getList(Integer distributionSite) {
+        LambdaQueryWrapper<IndexCarousel> wrapper = new LambdaQueryWrapper<>();
+        if (distributionSite!= null) {
+            wrapper.eq(IndexCarousel::getType, distributionSite);
+        }
+        wrapper.orderByDesc(IndexCarousel::getCreateTime);
+        List<IndexCarousel> list = baseMapper.selectList(wrapper);
+        return null;
+    }
 }
