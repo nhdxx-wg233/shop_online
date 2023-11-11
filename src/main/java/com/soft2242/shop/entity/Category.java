49ad3c9a03e2cd1f9b27bd53f796413492a375f9
wg-233 <18851716394@qq.com>
2023-11-11 16:16:32 +0800

    fix(Tab菜单): 不将逻辑删除、创建时间、更新时间返回

--- src/main/java/com/soft2242/shop/entity/Category.java
+++ src/main/java/com/soft2242/shop/entity/Category.java
@@ -46,13 +46,16 @@ public class Category {
     @ApiModelProperty("逻辑删除(0-未删除，1-已删除)")
     @TableField("delete_flag")
     @TableLogic
+    @JsonIgnore
     private Integer deleteFlag;
 
     @ApiModelProperty("创建时间")
     @TableField(value = "create_time", fill = FieldFill.INSERT)
+    @JsonIgnore
     private LocalDateTime createTime;
 
     @ApiModelProperty("更新时间")
     @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
+    @JsonIgnore
     private LocalDateTime updateTime;
 }
